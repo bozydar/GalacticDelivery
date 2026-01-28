1319c4f93fdd0cc4ca4d016554d33ae7dcca2ff2
Bozydar Sobczak <bozysob@gmail.com>
2026-01-29 09:00:21 +1300

    refact: REmoving unused methods from Result

--- GalacticDelivery.Common/Result.cs
+++ GalacticDelivery.Common/Result.cs
@@ -23,30 +23,6 @@ public readonly struct Result
     {
         return new Result(false, error ?? throw new ArgumentNullException(nameof(error)));
     }
-
-    public Result<T> Bind<T>(Func<Result<T>> binder)
-    {
-        if (binder is null)
-        {
-            throw new ArgumentNullException(nameof(binder));
-        }
-
-        return IsSuccess ? binder() : Result<T>.Failure(Error!);
-    }
-
-    public T Match<T>(Func<T> onSuccess, Func<Error, T> onFailure)
-    {
-        if (onSuccess is null)
-        {
-            throw new ArgumentNullException(nameof(onSuccess));
-        }
-        if (onFailure is null)
-        {
-            throw new ArgumentNullException(nameof(onFailure));
-        }
-
-        return IsSuccess ? onSuccess() : onFailure(Error!);
-    }
 }
 
 public readonly struct Result<T>
@@ -71,26 +47,6 @@ public readonly struct Result<T>
     public static Result<T> Failure(Error error)
     {
         return new Result<T>(false, default, error ?? throw new ArgumentNullException(nameof(error)));
-    }
-
-    public Result<TOut> Map<TOut>(Func<T, TOut> mapper)
-    {
-        if (mapper is null)
-        {
-            throw new ArgumentNullException(nameof(mapper));
-        }
-
-        return IsSuccess ? Result<TOut>.Success(mapper(Value!)) : Result<TOut>.Failure(Error!);
-    }
-
-    public Result<TOut> Bind<TOut>(Func<T, Result<TOut>> binder)
-    {
-        if (binder is null)
-        {
-            throw new ArgumentNullException(nameof(binder));
-        }
-
-        return IsSuccess ? binder(Value!) : Result<TOut>.Failure(Error!);
     }
 
     public TOut Match<TOut>(Func<T, TOut> onSuccess, Func<Error, TOut> onFailure)
